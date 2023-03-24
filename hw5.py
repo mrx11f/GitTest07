98752e563f2d2f11d558c41bd037d1bd02ca27fd
mrx11f <masanovrauan@gmail.com>
2023-03-24 19:26:56 +0300

    my commit

---
+++ hw5.py
@@ -0,0 +1,18 @@
+# Доп.задача №1
+# passwords = set()
+
+# password = input("Введите пароль: ")
+# passwords.add(password)
+
+# confirm_password = input("Подтвердите пароль: ")
+# passwords.add(confirm_password)
+
+# if len(password) < 8:
+#     print("Короткий пароль!")
+# elif "123" in password:
+#     print("Простой пароль!")
+# elif password != confirm_password:
+#     print("Различаются.")
+# else:
+#     print("OK")
+#     print("Пароль создан!")
